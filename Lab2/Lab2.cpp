ac770c0c3b8bb076137fb9b00f17a612588fd135
Nahorniy Roman <nahornyi.r22@fpm.dnu.edu.ua>
2023-10-01 15:23:25 +0300

    #createCopy #clearList #combineLists #commomElements

--- Lab2/Lab2.cpp
+++ Lab2/Lab2.cpp
@@ -17,6 +17,7 @@ int getLength(Node* head) {
 	}
 	return length;
 }
+
 struct List {
 	Node* head;
 	List() : head(nullptr) {}
@@ -59,24 +60,19 @@ struct List {
 			current->next = node;
 		}
 	}
-	void moveElement(int position, int steps) {
-		Node* element = nullptr;;
-		Node* previous = nullptr;
-		Node* next = nullptr;
-		for (int i = 0; i < steps; i++) {
+	void moveToRight(int position, int steps) {
+		for (int j = 0; j < steps; j++) {
 			
-			if (position == getLength(head)) {
-				position = 1;
-				element->next = head;
-				head = element;
-				next->next = nullptr;
-				element = nullptr;
-				previous = nullptr;
-				next = nullptr;
-				continue;
+			Node* current = head;
+			Node* element = nullptr;;
+			Node* previous = nullptr;
+			Node* next = nullptr;
+			int length = getLength(head);
+			if (position == length) {
+				cout << "Can't move element" << endl;
+				return;
 			}
-			Node* current = head;		
-			for (int i = 0; i < getLength(head); i++) {
+			for (int i = 0; i < length; i++) {
 				if (i == position - 2) {
 					previous = current;
 				}
@@ -90,14 +86,52 @@ struct List {
 			}
 			if (position != 1) {
 				previous->next = next;
-				
+				element->next = next->next;
+				next->next = element;
 			}
 			else {
+				element->next = next->next;
 				head = next;
+				next->next = element;
 			}
-			element->next = next->next;
-			next->next = element;
 			position++;
+		}
+	}
+	void moveToLeft(int position, int steps) {
+		for (int j = 0; j < steps; j++) {
+
+			Node* current = head;
+			Node* element = nullptr;;
+			Node* previousOfNext = nullptr;
+			Node* next = nullptr;
+			int length = getLength(head);
+			if (position == 1) {
+				cout << "Can't move element" << endl;
+				return;
+			}
+			for (int i = 0; i < length; i++) {
+				if (i == position - 2) {
+					next = current;
+				}
+				if (i == position - 1) {
+					element = current;
+				}
+				if (i == position - 3) {
+					previousOfNext = current;
+				}
+				current = current->next;
+			}
+			if (position != 2) {
+				next->next = element->next;
+				previousOfNext->next = element;
+				element->next = next;
+			}
+			else {
+				next->next = element->next;
+				head = element;
+				element->next = next;
+			}
+			position--;
 		}
 	}
 	void deleteByNumber(int position) {
@@ -132,28 +166,76 @@ struct List {
 			cout << "Wrong position" << endl;
 		}
 	}
-	void print() {
-		Node* p = head;
-		while (p) {
-			cout << p->surname << " ";
-			p = p->next;
+	void clearList() {
+		while (head) {
+			Node* node = head;
+			head = head->next;
+			delete node;
+		
 		}
 	}
+	void print() {
+		Node* node = head;
+		while (node) {
+			cout << node->surname << " ";
+			node = node->next;
+		}
+		cout << endl;
+	}
+	List createCopy() {
+		List newList;
+		Node* node = head;
+		while (node) {
+			newList.pushBack(node->surname);
+			node = node->next;
+		}
+		return newList;
+	}
 };
-
+void combineLists(List list1, List list2) {
+	Node* node = list1.head;
+	Node* head = list2.head;
+	while (node->next) {
+		node = node->next;
+	}
+	node->next = head;
+}
+List commonElements(List list1, List list2) {
+	Node* node1 = list1.head;
+	List newList;
+	while (node1) {
+		Node* node2 = list2.head;
+		while (node2) {		
+			if (node1->surname == node2->surname) {
+				newList.pushBack(node1->surname);
+			}
+			node2 = node2->next;
+		}
+		node1 = node1->next;
+	}
+	return newList;
+}
 int main()
 {
-	List list;
-	list.pushBack("1");
-	list.pushBack("2");
-	list.pushBack("3");
-	list.pushBack("4");
-	list.pushBack("5");
-	//list.pushByNumber(1, "Roman");
-	//list.pushByNumber(6, "Roman");
-	list.print();
-	cout << endl;
-	list.print();
-
+	List list1;
+	list1.pushBack("1");
+	list1.pushBack("2");
+	list1.pushBack("3");
+	list1.pushBack("4");
+	list1.pushBack("5");
+	list1.print();
+	List list2;
+	list2.pushBack("6");
+	list2.pushBack("7");
+	list2.pushBack("8");
+	list2.pushBack("9");
+	list2.pushBack("10");
+	list2.print();
+	combineLists(list1, list2);
+	list1.print();
+	List list3 = commonElements(list1, list2);
+	list3.print();
+	List list4 = list3.createCopy();
+	list4.print();
 }
 
